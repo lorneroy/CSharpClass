8153687e3d0fba79728c52d85d39cdf697b33d52
lorneroy <lorne.roy@philips.com>
2016-04-19 18:01:11 -0700

    Ready to submit Spring assignment 01

---
+++ CSHP 812 Sp16/Lorne Roy Assignment01/Lorne Roy Assignment01/JavaScript.js
@@ -0,0 +1,35 @@
+﻿//This function validates the first and last name inputs on the form
+function validateInputs()
+{
+    var firstName, lastName, response = "";
+
+    var errorSet = false;
+
+    firstName = document.getElementById("firstName").value;
+
+    lastName = document.getElementById("lastName").value;
+    
+    if (firstName == null || firstName == "")
+    {
+        response += " Please fill out first name.";
+
+        errorSet = true;
+    }
+    if (lastName == null || lastName == "")
+    {
+        response += " Please fill out last name";
+
+        errorSet = true;
+    }
+
+    if (errorSet)
+    {
+        document.getElementById("nameValidationMessage").innerHTML = response.trim();
+
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
